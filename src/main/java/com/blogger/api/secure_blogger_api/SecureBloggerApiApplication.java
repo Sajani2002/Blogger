ef8021f535fa288b04611d0fce58fccd309b03b3
Sajani <hasupeksha2002@gmail.com>
2025-11-07 18:37:00 +0530

    setup done

---
+++ src/main/java/com/blogger/api/secure_blogger_api/SecureBloggerApiApplication.java
@@ -0,0 +1,13 @@
+package com.blogger.api.secure_blogger_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureBloggerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureBloggerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogger/api/secure_blogger_api/SecureBloggerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogger.api.secure_blogger_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureBloggerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
